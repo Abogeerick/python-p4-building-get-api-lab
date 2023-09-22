d425afc1ebf88b510745d1a06ea261bf23fbbd63
Abogeerick <abogeerick@gmail.com>
2023-09-22 05:39:45 +0300

    Complete assignment

--- server/app.py
+++ server/app.py
@@ -19,20 +19,49 @@ def index():
     return '<h1>Bakery GET API</h1>'
 
 @app.route('/bakeries')
-def bakeries():
-    return ''
+def get_bakeries():
+    bakeries = Bakery.query.all()
+    response = Response(response=json.dumps([bakery.serialize() for bakery in bakeries]), 
+                        status=200, 
+                        mimetype='application/json')
+    return response
 
 @app.route('/bakeries/<int:id>')
-def bakery_by_id(id):
-    return ''
+def get_bakery_by_id(id):
+    bakery = Bakery.query.get(id)
+    if bakery is None:
+        response = Response(response=json.dumps({"error": "Bakery not found"}), 
+                            status=404, 
+                            mimetype='application/json')
+        return response
+    response = Response(response=json.dumps(bakery.serialize_with_baked_goods()), 
+                        status=200, 
+                        mimetype='application/json')
+    return response
 
-@app.route('/baked_goods/by_price')
-def baked_goods_by_price():
-    return ''
 
-@app.route('/baked_goods/most_expensive')
-def most_expensive_baked_good():
-    return ''
+@app.route('/baked_goods/by_price', methods=['GET'])
+def get_baked_goods_by_price():
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    response = Response(response=json.dumps([good.serialize() for good in baked_goods]), 
+                        status=200, 
+                        mimetype='application/json')
+    return response
+
+
+@app.route('/baked_goods/most_expensive', methods=['GET'])
+def get_most_expensive_baked_good():
+    most_expensive_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive_good is None:
+        response = Response(response=json.dumps({"error": "No baked goods found"}), 
+                            status=404, 
+                            mimetype='application/json')
+        return response
+    response = Response(response=json.dumps(most_expensive_good.serialize()), 
+                        status=200, 
+                        mimetype='application/json')
+    return response
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,10 +11,22 @@ db = SQLAlchemy(metadata=metadata)
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
+    serialize_rules = ('-bakedgoods.bakery')
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ccreated_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+    bakedgoods = db.relationship('BakedGood', backref='bakery')
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
+    serialize_rules = ('-bakery.bakedgoods')
+
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
